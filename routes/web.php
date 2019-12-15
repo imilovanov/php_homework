7d38396b4deb7faea7f7db1098ed832b625c7a0d
Ivan Milovanov <milovanov.ivan.94@gmail.com>
2019-12-15 15:36:55 +0700

    Task 10

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test/one', 'TestController@getOne');
+Route::prefix('test')->group(function () {
+    Route::get('/test/one', 'TestController@getOne');
 
-Route::post('/test/one', 'TestController@setOne');
+    Route::post('/test/one', 'TestController@setOne');
+});
