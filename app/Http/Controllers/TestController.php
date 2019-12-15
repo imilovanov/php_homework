81a09d250febd9bbbec0a5d6dcd4c09967cb3e39
Ivan Milovanov <milovanov.ivan.94@gmail.com>
2019-12-15 14:53:53 +0700

    Task 4

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,9 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+class TestOne
+{
+    private $name;
+    private $age;
+    private $boolProperty;
+
+    public function getPropertiesTypes() {
+        return [
+            'name' => 'string',
+            'age' => 'int',
+            'boolProperty' => 'boolean'
+        ];
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function setAge($age) {
+        $this->age = $age;
+    }
+
+    public function setBoolProperty($boolProperty) {
+        $this->boolProperty = $boolProperty;
+    }
+};
+
 class TestController extends Controller
 {
     public function getOne() {
-        return 'test';
+        $testOne = new TestOne();
+        return $testOne->getPropertiesTypes();
     }
 }
