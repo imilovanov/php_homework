4e6dd7b3c757a86b2800296c5b2d10dab9062f2c
Ivan Milovanov <milovanov.ivan.94@gmail.com>
2019-12-15 15:33:15 +0700

    Task 9

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,55 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\TestBasicService;
 use Illuminate\Http\Request;
-
-class TestOne
-{
-    private $name;
-    private $age;
-    private $boolProperty;
-
-    public function getPropertiesTypes() {
-        return [
-            'name' => 'string',
-            'age' => 'int',
-            'boolProperty' => 'boolean'
-        ];
-    }
-
-    public function getPropertiesValues() {
-        return [
-            'name' => $this->name,
-            'age' => $this->age,
-            'boolProperty' => $this->boolProperty
-        ];
-    }
-
-    public function setName($name) {
-        $this->name = $name;
-    }
-
-    public function setAge($age) {
-        $this->age = $age;
-    }
-
-    public function setBoolProperty($boolProperty) {
-        $this->boolProperty = $boolProperty;
-    }
-};
 
 class TestController extends Controller
 {
-    public function getOne() {
-        $testOne = new TestOne();
-        return $testOne->getPropertiesTypes();
+    private $testBasicService;
+
+    public function __construct(TestBasicService $service)
+    {
+        $this->testBasicService = $service;
     }
 
-    public function setOne(Request $request) {
-        $testOne = new TestOne();
-        $testOne->setName($request->name);
-        $testOne->setAge($request->age);
-        $testOne->setBoolProperty($request->boolProperty);
-        return $testOne->getPropertiesValues();
+    public function getOne()
+    {
+        return $this->testBasicService->getTestOnePropertiesTypes();
+    }
+
+    public function setOne(Request $request)
+    {
+        return $this->testBasicService->setTestOnePropertiesValues($request->name, $request->age, $request->boolProperty);
     }
 }
